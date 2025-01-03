ef8d0cc8a0e7e47ed0a3d7faea039624db0d2d76
root <root@ip-172-31-87-15.ec2.internal>
2025-01-03 17:50:56 +0000

    added another script

---
+++ 23_arguments.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+
+echo "Script name is $0"
+echo "this is first argument $1"
+echo "this is second argument $2"
+
+
+
+
+echo "Total number of arguments are $#"
+
+
+
+echo "All arguments passed are $*"
+echo "All arguments passed are $@"
+
+
+echo "exit status code of last commnd is $?"
